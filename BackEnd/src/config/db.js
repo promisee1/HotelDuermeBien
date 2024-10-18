59660b9111b975aa92f5d41d3710be1da0c9fe1f
Jeriam <jeriamrogers@gmail.com>
2024-10-18 12:51:36 -0300

    disposicion home y header recepcionista

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -2,8 +2,9 @@ import { Sequelize } from 'sequelize';
 
 
 //Constante de conexión a la base de datos
-const sequelize = new Sequelize('db_duermebien', 'benjaja', 'benjadev', {
+const sequelize = new Sequelize('db_duermebien', 'root', '', {
   host: 'localhost',
+  port: 3307,
   dialect: 'mysql' // Cambia el dialecto si usas otra base de datos (por ejemplo, 'postgres', 'sqlite')
 });
 
